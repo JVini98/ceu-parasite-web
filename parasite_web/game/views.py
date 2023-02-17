2b48ea3ab58d53e6f93b3c0c41c92f2044b1c477
gemapatricia <gp.herrera@usp.ceu.es>
2023-02-17 18:42:50 +0100

    New game application created

---
+++ parasite_web/game/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def manipulateImage(request):
+    return HttpResponse("Accedo a mi aplicación")
