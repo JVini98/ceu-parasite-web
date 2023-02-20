b5fc0f1d3aaba36c28e296d4d3aa4dbf50d60f3e
gemapatricia <gp.herrera@usp.ceu.es>
2023-02-20 18:47:06 +0100

    Cleaning code

--- parasite_web/game/static/javascripts/crop.js
+++ parasite_web/game/static/javascripts/crop.js
@@ -1,10 +1,3 @@
-/*let canvas = document.getElementById("myCanvas");
-let ctx = canvas.getContext("2d");
-        
-let img = new Image();
-img.src = "../static/images/ameba.jpg";
-ctx.drawImage(img, 0, 0, canvas.width, canvas.height); */
-
 const image = document.getElementById('gameImage');
 const button = document.getElementById('cropButton');
 const parasites = document.getElementById('parasites');
