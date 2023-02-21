67b9639b0adfd49e2e3e477db09b3abd8c362eec
gemapatricia <gp.herrera@usp.ceu.es>
2023-02-21 15:00:11 +0100

    Showing image properties

--- parasite_web/game/static/javascripts/crop.js
+++ parasite_web/game/static/javascripts/crop.js
@@ -2,8 +2,11 @@ const image = document.getElementById('gameImage');
 const buttonCrop = document.getElementById('cropButton');
 const buttonSave = document.getElementById('saveButton');
 const parasitesForm = document.getElementById('parasitesForm');
-const annotation = document.getElementById('annotation');
+const annotationForm = document.getElementById('annotation');
+
 let url;
+let imagePosition;
+let annotationSave;
 
 const cropper = new Cropper(image, {
   aspectRatio: 16 / 9,
@@ -19,21 +22,29 @@ const cropper = new Cropper(image, {
   },
 });
 
+// crop image on click
 buttonCrop.addEventListener('click', ()=>{
   cropper.getCroppedCanvas().toBlob((blob) => {
+    imagePosition = cropper.getData();
     url = URL.createObjectURL(blob);
     parasitesForm.innerHTML = `<image src="${url}" width="150px" class="imageCentered marginBotton"/>`;
   });
 });
 
+// temporary save image on click
 buttonSave.addEventListener('click', ()=>{
-  parasitesResponse.innerHTML = `<image src="${url}" width="150px" class="margin"/>`;
+  annotationSave = annotationForm.value;
+  parasitesResponse.innerHTML = `<image src="${url}" width="150px" class="margin"/>
+                                 <p>${annotationSave}</p>
+                                 <p>x:${imagePosition.x}; y:${imagePosition.y}; width:${imagePosition.width}; height:${imagePosition.height}`;
+  annotationForm.value="";
 });
 
+// change state button according to input value
 buttonSave.disabled=true;
 
-annotation.addEventListener("input", ()=>{
-  if (annotation.value === "") {
+annotationForm.addEventListener("input", ()=>{
+  if (annotationForm.value === "") {
     buttonSave.disabled = true; 
   } else {
     buttonSave.disabled = false; 
