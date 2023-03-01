b5a9d71328ab2dce8c30814708d500c700f18429
gemapatricia <gp.herrera@usp.ceu.es>
2023-03-01 10:59:09 +0100

    Working on processing json

--- parasite_web/game/views.py
+++ parasite_web/game/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import CroppedImage
+import json
 
 # Create your views here.
 def manipulateImage(request):
     if request.method == "POST":
-        json = request.POST.get('json')
+        jsonReceived = request.POST.get('json')
+        json2 = json.loads(jsonReceived)
         message = "Successfully sent to the server"
-        return HttpResponse(message)
+        return HttpResponse(json2)
     else:
         image = CroppedImage.objects.get(pk=2)
         return render(request=request, template_name="game.html", context={'image': image})
