b4a05f3b9ce00982c6047830fc95d11762384e2d
gemapatricia <gp.herrera@usp.ceu.es>
2023-03-07 12:09:40 +0100

    Prototype parsing json server

--- parasite_web/game/views.py
+++ parasite_web/game/views.py
@@ -8,9 +8,13 @@ import json
 def manipulateImage(request):
     if request.method == "POST":
         jsonReceived = request.POST.get('json')
+
         if (jsonReceived != ""):
             json2 = json.loads(jsonReceived)
+            #for dict in json2:
+                #insert(dict["url"], dict["annotation"]...)
             return JsonResponse({'message': "Successfully sent to the server"})
+            #return JsonResponse({'message': json2[0]["url"]})
     else:
         image = CroppedImage.objects.get(pk=2)
         return render(request=request, template_name="game.html", context={'image': image})
