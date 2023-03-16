3b6d6187529f2cdddbd445350d039074cc488ff6
gemapatricia <gp.herrera@usp.ceu.es>
2023-03-16 11:29:27 +0100

    Duplicated model eliminated

--- parasite_web/game/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import CroppedImage
-
-class CroppedImageForm(forms.ModelForm):
-    class Meta:
-        model = CroppedImage
-        fields = ('file', )
--- parasite_web/game/views.py
+++ parasite_web/game/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
-from .models import CroppedImage
+from uploads.models import ParasiteImage
 import json
 
 # Create your views here.
@@ -16,7 +16,7 @@ def manipulateImage(request):
             return JsonResponse({'message': "Successfully sent to the server"})
             #return JsonResponse({'message': json2[0]["url"]})
     else:
-        image = CroppedImage.objects.get(pk=2)
+        image = ParasiteImage.objects.get(pk=4)
         return render(request=request, template_name="game.html", context={'image': image})
 
 
