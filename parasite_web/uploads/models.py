d80d128d327cf01db5aa99383fe9016d4c13958a
gemapatricia <gp.herrera@usp.ceu.es>
2023-03-20 12:37:07 +0100

    Provisional model Parasite

--- parasite_web/uploads/models.py
+++ parasite_web/uploads/models.py
@@ -7,3 +7,22 @@ class Photograph(models.Model):
 
     def __str__(self):
         return str(self.pk)
+
+class Parasite(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return str(self.pk)
+
+if (not Parasite.objects.exists()):
+    Parasite(name="Entamoeba").save()
+    Parasite(name="Ascaris lumbricoides").save()
+    Parasite(name="Balantidium coli").save()
+    Parasite(name="Diphyllobothrium latum").save()
+    Parasite(name="Entamoeba coli").save()
+    Parasite(name="Enterobius vermicularis").save()
+    Parasite(name="Giardia").save()
+    Parasite(name="Hymenolepis nana").save()
+    Parasite(name="Taenia").save()
+    Parasite(name="Trichuris trichura").save()
+    Parasite(name="Hookworm").save()
