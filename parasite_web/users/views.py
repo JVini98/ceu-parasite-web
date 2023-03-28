c1dbf2dec448c90f361d60394cec2fa277e80477
gemapatricia <gp.herrera@usp.ceu.es>
2023-03-28 13:09:21 +0200

    Simple UI registration form

--- parasite_web/users/views.py
+++ parasite_web/users/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import SignUpForm
 
 # Create your views here.
 def registerUser(request):
-    return HttpResponse("Todo ha ido bien")
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirect to a success page
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
