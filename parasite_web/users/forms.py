65b115e5f6fdab48e622151b64f4699c780b19db
gemapatricia <gp.herrera@usp.ceu.es>
2023-03-28 19:47:31 +0200

    ALl form inputs included

--- parasite_web/users/forms.py
+++ parasite_web/users/forms.py
@@ -6,7 +6,9 @@ class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
     last_name = forms.CharField(max_length=30, required=True, help_text='Required.')
     email = forms.EmailField(max_length=254, required=True, help_text='Required. Enter a valid email address.')
+    password1 = forms.CharField(widget=forms.PasswordInput(), required=True, help_text='Required.')
+    password2 = forms.CharField(widget=forms.PasswordInput(), required=True, help_text='Required.')
 
-    '''class Meta:
+    class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )'''
+        fields = ['first_name', 'last_name', 'email', 'password1', 'password2', ]
