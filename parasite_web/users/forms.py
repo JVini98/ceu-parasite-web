6fb6b97862cc72ffdac413b487f9ff2b0e8aebe1
gemapatricia <gp.herrera@usp.ceu.es>
2023-03-29 01:03:04 +0200

    Form Class included in HTML

--- parasite_web/users/forms.py
+++ parasite_web/users/forms.py
@@ -3,11 +3,58 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
+    '''first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
     last_name = forms.CharField(max_length=30, required=True, help_text='Required.')
     email = forms.EmailField(max_length=254, required=True, help_text='Required. Enter a valid email address.')
     password1 = forms.CharField(widget=forms.PasswordInput(), required=True, help_text='Required.')
-    password2 = forms.CharField(widget=forms.PasswordInput(), required=True, help_text='Required.')
+    password2 = forms.CharField(widget=forms.PasswordInput(), required=True, help_text='Required.')'''
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["first_name"].widget.attrs.update({
+            'id': 'first_name',
+            'class': 'form-input',
+            'name': 'first_name',
+            'required': 'true',
+            'type': 'text',
+            'placeholder': 'Gemma'
+        })
+        self.fields["last_name"].widget.attrs.update({
+            'id': 'last_name',
+            'class': 'form-input',
+            'name': 'last_name',
+            'required': 'true',
+            'type': 'text',
+            'placeholder': 'Smith'
+        })
+        self.fields["email"].widget.attrs.update({
+            'id': 'email',
+            'class': 'form-input',
+            'name': 'email',
+            'required': 'true',
+            'type': 'email',
+            'placeholder': 'gemmasmith@mail.com'
+        })
+        self.fields["password1"].widget.attrs.update({
+            'id': 'password1',
+            'class': 'form-input',
+            'name': 'password1',
+            'required': 'true',
+            'type': 'password',
+            'placeholder': 'Password',
+            'minlength': '8',
+            'maxlength': '22'
+        })
+        self.fields["password2"].widget.attrs.update({
+            'id': 'password2',
+            'class': 'form-input',
+            'name': 'password2',
+            'required': 'true',
+            'type': 'password',
+            'placeholder': 'Confirm Password',
+            'minlength': '8',
+            'maxlength': '22'
+        })
 
     class Meta:
         model = User
