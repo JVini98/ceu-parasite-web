1b6680a42d144de030b6f6f72241dab941bcae39
gemapatricia <gp.herrera@usp.ceu.es>
2023-04-09 18:14:10 +0200

    Displaying all data in Admin Site

--- parasite_web/users/admin.py
+++ parasite_web/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'is_active', 'is_staff')
+
+admin.site.register(User, UserAdmin)
