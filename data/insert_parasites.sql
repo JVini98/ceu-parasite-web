958566a2791d17b5a65f1e0e1c1bca0f3a9c5108
gemapatricia <gp.herrera@usp.ceu.es>
2023-04-23 19:52:52 +0200

    SQL script to insert parasites

---
+++ data/insert_parasites.sql
@@ -0,0 +1,16 @@
+/* 
+1. Open MySQL terminal
+2. insert: source path/to/file
+*/
+use parasites;
+INSERT INTO uploads_parasite (name) VALUES ('Entamoeba');
+INSERT INTO uploads_parasite (name) VALUES ('Ascaris lumbricoides');
+INSERT INTO uploads_parasite (name) VALUES ('Balantidium coli');
+INSERT INTO uploads_parasite (name) VALUES ('Diphyllobothrium latum');
+INSERT INTO uploads_parasite (name) VALUES ('Entamoeba coli');
+INSERT INTO uploads_parasite (name) VALUES ('Enterobius vermicularis');
+INSERT INTO uploads_parasite (name) VALUES ('Giardia');
+INSERT INTO uploads_parasite (name) VALUES ('Hymenolepis nana');
+INSERT INTO uploads_parasite (name) VALUES ('Taenia');
+INSERT INTO uploads_parasite (name) VALUES ('Trichuris trichura');
+INSERT INTO uploads_parasite (name) VALUES ('Hookworm');
