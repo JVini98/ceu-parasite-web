c2df86829647a54d13e98b7c7eb9a2a26c8caffe
gemapatricia <gp.herrera@usp.ceu.es>
2023-04-26 21:55:29 +0200

    Refactor deleting image EXIF metadata

--- parasite_web/game/views.py
+++ parasite_web/game/views.py
@@ -38,7 +38,7 @@ def manipulateImage(request):
                                                 width=dict["width"], 
                                                 height=dict["height"], 
                                                 user=user,
-                                                photograph=Photograph.objects.first(),
+                                                photograph=Photograph.objects.get(pk=5),
                                                 parasite=retrieveIdParasite(dict["annotation"])
                                                 )
                 identification.save()
@@ -46,41 +46,40 @@ def manipulateImage(request):
     else:
         # If the user is logged in
         if ('user' in request.session):
-            image = Photograph.objects.first()
-            imageDisplay, executed = deleteEXIF(image)
+            image = Photograph.objects.get(pk=5)
+            executed, imageDisplay = deleteEXIF(image)
             # Image with EXIF metadata (JPEG)
             if executed:
                 buffer = BytesIO()
                 imageDisplay.save(buffer, format="JPEG")
                 buffer.seek(0)
                 mime_type = "image/jpeg"
-                contents = buffer.getvalue().hex()
-                contents_bytes = binascii.a2b_hex(contents)
-                contents_base64 = base64.b64encode(contents_bytes).decode()
-                parasites = Parasite.objects.values_list('name', flat=True)
-                form = ReportPhotographForm()
-                return render(request=request, template_name="game.html", context={"image": f"data:{mime_type};base64,{contents_base64}", 'parasites': parasites, 'form': form})
+                contents = buffer.getvalue()
+                contents_base64 = base64.b64encode(contents).decode()
+                imageRender = f"data:{mime_type};base64,{contents_base64}"
             # Image with no EXIF metadata
             else: 
-                parasites = Parasite.objects.values_list('name', flat=True)
-                form = ReportPhotographForm()
-                return render(request=request, template_name="game.html", context={"image": imageDisplay.path.url, 'parasites': parasites, 'form': form})
+                imageRender = imageDisplay.path.url
+            parasites = Parasite.objects.values_list('name', flat=True)
+            form = ReportPhotographForm()
+            return render(request=request, template_name="game.html", context={"image": imageRender, 'parasites': parasites, 'form': form})
         # Display error message
         else: 
             error = 'To access the game section, you need to login'
             return redirect(f'/?error={error}')
 
-# Delete EXIF metadata to display the image with no position
+# Delete EXIF metadata from a JPEG file to display the image with no unwanted rotation
 def deleteEXIF(imageDB):
-    imageRecovered = imageDB.path
-    imageBytes = imageRecovered.read()
+    imagePath = imageDB.path
+    imageBytes = imagePath.read()
     imageToChange = Image.open(BytesIO(imageBytes))
+    # Check if the image has EXIF metadata to delete it
     if "exif" in imageToChange.info:
         exifDict = piexif.load(imageToChange.info["exif"])
         exifBytes = piexif.dump(exifDict)
         del imageToChange.info["exif"]
-        output = BytesIO()
-        imageToChange.save(output, format="JPEG", exif=exifBytes)
-        return imageToChange, True
-    return imageDB, False
+        buffer = BytesIO()
+        imageToChange.save(buffer, format="JPEG", exif=exifBytes)
+        return True, imageToChange
+    return False, imageDB 
 
