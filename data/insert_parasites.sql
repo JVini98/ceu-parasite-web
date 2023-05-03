aa38648967caca054049581ae89e0108a538850e
gemapatricia <gp.herrera@usp.ceu.es>
2023-05-03 20:56:54 +0200

    Parasites data updated

--- data/insert_parasites.sql
+++ data/insert_parasites.sql
@@ -3,14 +3,16 @@
 2. insert: source path/to/file
 */
 use parasites;
-INSERT INTO uploads_parasite (name) VALUES ('Entamoeba');
+INSERT INTO uploads_parasite (name) VALUES ('Ameba');
 INSERT INTO uploads_parasite (name) VALUES ('Ascaris lumbricoides');
 INSERT INTO uploads_parasite (name) VALUES ('Balantidium coli');
 INSERT INTO uploads_parasite (name) VALUES ('Diphyllobothrium latum');
-INSERT INTO uploads_parasite (name) VALUES ('Entamoeba coli');
+INSERT INTO uploads_parasite (name) VALUES ('Enterobius');
 INSERT INTO uploads_parasite (name) VALUES ('Enterobius vermicularis');
 INSERT INTO uploads_parasite (name) VALUES ('Giardia');
 INSERT INTO uploads_parasite (name) VALUES ('Hymenolepis nana');
 INSERT INTO uploads_parasite (name) VALUES ('Taenia');
 INSERT INTO uploads_parasite (name) VALUES ('Trichuris trichura');
-INSERT INTO uploads_parasite (name) VALUES ('Hookworm');
+INSERT INTO uploads_parasite (name) VALUES ('Uncinaria');
+-- INSERT INTO uploads_parasite (name) VALUES ('Entamoeba');
+-- INSERT INTO uploads_parasite (name) VALUES ('Hookworm');
