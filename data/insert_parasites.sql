00ec41133acab2d5ff71fa1116e83deb349f140c
gemapatricia <gp.herrera@usp.ceu.es>
2023-05-04 13:46:48 +0200

    Duplicated parasite eliminated

--- data/insert_parasites.sql
+++ data/insert_parasites.sql
@@ -7,7 +7,6 @@ INSERT INTO uploads_parasite (name) VALUES ('Ameba');
 INSERT INTO uploads_parasite (name) VALUES ('Ascaris lumbricoides');
 INSERT INTO uploads_parasite (name) VALUES ('Balantidium coli');
 INSERT INTO uploads_parasite (name) VALUES ('Diphyllobothrium latum');
-INSERT INTO uploads_parasite (name) VALUES ('Enterobius');
 INSERT INTO uploads_parasite (name) VALUES ('Enterobius vermicularis');
 INSERT INTO uploads_parasite (name) VALUES ('Giardia');
 INSERT INTO uploads_parasite (name) VALUES ('Hymenolepis nana');
