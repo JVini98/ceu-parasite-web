f9613e9ad2eef8c9719bc58873e574fc6abbed6b
gemapatricia <gp.herrera@usp.ceu.es>
2023-06-11 17:27:19 +0200

    Cookies Policy (no cookies on deployment)

--- parasite_web/home/urls.py
+++ parasite_web/home/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('account_settings', views.account_settings, name="account_settings"),
     path('update_user_name', views.update_user_name, name="update_user_name"),
     path('update_password', views.update_password, name="update_password"),
-    path('delete_user', views.delete_user, name="delete_user")
+    path('delete_user', views.delete_user, name="delete_user"),
+    path('cookies', views.cookies, name="cookies")
 ]
