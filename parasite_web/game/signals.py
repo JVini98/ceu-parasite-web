61d9d8b025d0883bb028d43cd0570fb54a2b0305
gemapatricia <gp.herrera@usp.ceu.es>
2023-06-18 18:03:33 +0200

    Signal script commented

--- parasite_web/game/signals.py
+++ parasite_web/game/signals.py
@@ -10,17 +10,17 @@ identifications = []
 def log_identification(sender, instance, created, **kwargs):
     if created:
         print(f'New identification created: {instance.id}')
-        # Define the number of identifications per image to launch the cluster algorithm
-        number_indentifications_per_image = 2
-        # Get the photographs that match the number of identifications per image 
-        photographs_valid = Identification.objects.values('photograph')\
-            .annotate(num_photos=Count('photograph'))\
-            .filter(num_photos__gte=number_indentifications_per_image)
-        # print(photographs_valid)
-        # Get Querysets with the all the identifications per image
-        for photograph_valid in photographs_valid:
-            identifications_per_image = Identification.objects.filter(photograph=photograph_valid['photograph']).values('coordinateX', 'coordinateY', 'width', 'height', 'parasite')
-            identifications.append(identifications_per_image)
-        # print(identifications)
+        # # Define the number of identifications per image to launch the cluster algorithm
+        # number_indentifications_per_image = 2
+        # # Get the photographs that match the number of identifications per image 
+        # photographs_valid = Identification.objects.values('photograph')\
+        #     .annotate(num_photos=Count('photograph'))\
+        #     .filter(num_photos__gte=number_indentifications_per_image)
+        # # print(photographs_valid)
+        # # Get Querysets with the all the identifications per image
+        # for photograph_valid in photographs_valid:
+        #     identifications_per_image = Identification.objects.filter(photograph=photograph_valid['photograph']).values('coordinateX', 'coordinateY', 'width', 'height', 'parasite')
+        #     identifications.append(identifications_per_image)
+        # # print(identifications)
     else:
         print('Identification updated, so nothing is done')
