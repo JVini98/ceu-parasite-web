4e0fd6f613ab760d8a7a3217bb8b34304a890c90
gemapatricia <gp.herrera@usp.ceu.es>
2023-06-18 16:29:32 +0200

    Test 1 ok

--- parasite_web/game/tests.py
+++ parasite_web/game/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from clustering import get_clusters_per_image
+
+import unittest
 
 # Create your tests here.
+class TestClustering(unittest.TestCase):
+    def test_one_cluster(self):
+        identifications = []
+        identifications.append({'coordinateX': 1000, 'coordinateY': 1700, 'width': 200, 'height':100, 'parasite': "A"})
+        identifications.append({'coordinateX': 1050, 'coordinateY': 1650, 'width': 150, 'height':110, 'parasite': "A"})
+        
+        expected_clusters = [["1025.0", "1675.0", "175.0", "105.0", "A"]]
+        calculated_cluster = get_clusters_per_image(identifications)
+        
+        self.assertEqual(expected_clusters, calculated_cluster)
+
+if __name__ == "__main__":
+    unittest.main()
+
