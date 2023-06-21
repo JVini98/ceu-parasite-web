ab999b6a2152d54d5fa4716b861e0cde94584502
gemapatricia <gp.herrera@usp.ceu.es>
2023-06-21 20:21:49 +0200

    Error when deleting user solved

--- parasite_web/home/static/javascripts/confirmation.js
+++ parasite_web/home/static/javascripts/confirmation.js
@@ -2,8 +2,8 @@ const buttonDelete = document.getElementById('delete_buttom');
 const formDelete = document.getElementById('delete_form')
 
 // Show confirm message
-buttonDelete.addEventListener('click', ()=>{
+buttonDelete.addEventListener('click', (e)=>{
+    e.preventDefault();
     var confirmed = confirm("Are you sure you want to delete the user?");
-
     if (confirmed) formDelete.submit();
 });
