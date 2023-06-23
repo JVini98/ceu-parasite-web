83b042d9e8cc791bebcdd1e92863e4e4bd5d2531
Jaime Viniegra <jimmyviniegra@gmail.com>
2023-06-23 17:38:24 +0000

    bug arreglado activar y desactivar modo debug en la inferencia v2

--- parasite_web/parasite_web/settings.py
+++ parasite_web/parasite_web/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 SECRET_KEY = os.getenv('SECRET_KEY') 
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG')=='True'
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(' ') 
 AI_SERVER = os.getenv('AI_SERVER')
 
